be6e2ab0c696cff909d2bd50eee6ff1f134106f3
brian <bamborszcz@gmail.com>
2020-08-27 10:43:55 -0300

    head up

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,30 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatSliderModule } from '@angular/material/slider';
+import { HeadButtonsComponent } from './components/head/head-buttons/head-buttons.component';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { MatButtonToggleModule } from '@angular/material/button-toggle';
+
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeadButtonsComponent
   ],
   imports: [
+    MatButtonToggleModule,
+    MatButtonModule,
+    MatIconModule,
+    MatToolbarModule,
+    MatSliderModule,
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/head/head-buttons/head-buttons.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-head-buttons',
+  templateUrl: './head-buttons.component.html',
+  styleUrls: ['./head-buttons.component.css']
+})
+export class HeadButtonsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
